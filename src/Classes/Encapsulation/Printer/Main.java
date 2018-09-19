5e6520cffcc68ac88064ccfc46ab3699215d31f5
Memnoc <smartdroidesign@gmail.com>
2018-09-19 09:45:59 +0100

    - Encapsulation challenge completed (corrected, all good!)

--- src/Classes/Encapsulation/Printer/Main.java
+++ src/Classes/Encapsulation/Printer/Main.java
@@ -4,6 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
         // Everything working, logic seems solid
+        Printer printer = new Printer(50,false);
+        System.out.println("Initial page count = " + printer.getNumberOfPagesPrinted());
+        System.out.println("Pages printed was: ");
+        printer.printPages(4);
+        System.out.println("New total count for printer is: ");
+        System.out.println(printer.getNumberOfPagesPrinted());
+
+        System.out.println("Pages printed was: ");
+        printer.printPages(2);
+        System.out.println("New total count for printer is: ");
+        System.out.println(printer.getNumberOfPagesPrinted());
+
+
+
+
+
+
 
     }
 }
--- src/Classes/Encapsulation/Printer/Printer.java
+++ src/Classes/Encapsulation/Printer/Printer.java
@@ -5,15 +5,15 @@ public class Printer {
     private int numberOfPagesPrinted;
     private boolean isDuplex = false;
 
-    public Printer(int tonerLevel, int numberOfPagesPrinted, boolean isDuplex) {
+    public Printer(int tonerLevel, boolean isDuplex) {
         this.tonerLevel = tonerLevel;
-        this.numberOfPagesPrinted = numberOfPagesPrinted;
+        this.numberOfPagesPrinted = 0;
         this.isDuplex = isDuplex;
     }
 
     public void printPages(int pagesToPrint) {
         if(this.isDuplex){
-            int pagesPrinted = this.numberOfPagesPrinted += (pagesToPrint / 2);
+            int pagesPrinted = this.numberOfPagesPrinted += (pagesToPrint / 2) + (pagesToPrint % 2);
             System.out.println("Pages to print: " + pagesToPrint + " Printed pages: " + pagesPrinted);
         } else {
             int pagesPrinted =  this.numberOfPagesPrinted += pagesToPrint;
@@ -22,12 +22,12 @@ public class Printer {
     }
 
     public void fillToner(int quantity) {
-        int total = this.tonerLevel += quantity;
-        System.out.println("The toner level got refilled to " + total + " The current level of toner is " + this.tonerLevel);
         if (this.tonerLevel > 100) {
             this.tonerLevel = 100;
             System.out.println("The toner is full: current level => " + this.tonerLevel);
         } else {
+            int total = this.tonerLevel += quantity;
+            System.out.println("The toner level got refilled to " + total + " The current level of toner is " + this.tonerLevel);
             System.out.println("Toner level: " + this.tonerLevel);
         }
 
